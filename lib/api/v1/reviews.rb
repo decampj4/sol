e937d847d577ab740e65ffb6ae42eb162bbc7f50
Justin DeCamp <decampj4@gmail.com>
2016-01-31 16:52:24 -0600

    Adding a base API and updating various other configs

---
+++ lib/api/v1/reviews.rb
@@ -0,0 +1,22 @@
+module API
+  module V1
+    class Reviews < Grape::API
+      version 'v1'
+      format :json
+
+      helpers do
+      end
+
+      resource :reviews do
+        desc 'Return all reviews'
+
+        params do
+        end
+
+        get do
+          {"Test" : 'response'}
+        end
+      end
+    end
+  end
+end
